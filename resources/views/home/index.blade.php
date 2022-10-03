b756bd5830a0546b02dc7319b01ef186d21a9923
patrik pawa <patrikwilliam001@gmail.com>
2022-10-03 11:26:08 +0800

    add new file

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.main')
+
+@section('content')
+    <main class="container">
+      <div class="text-center">
+         <img src="img-bg/ss.png" class="mt-4" alt="" style="max-width: 160px;">
+         <div class="title mt-3">
+            <h5 class="fw-bold">DEWAN PERWAKILAN RAKYAT DAERAH</h5>
+            <h5 class="fw-bold">PROVINSI SULAWESI SELATAN</h5>
+         </div>
+
+         <div class="button-aspirasi">
+            <a href="" class="btn btn-primary px-4 fw-bold my-3">Aspirasi</a>
+         </div>
+
+         <div class="row mt-4">
+            <div class="col-md-3">
+                  <div class="card text-dark bg-white mx-3 mb-3" style="max-width: 18rem;">
+                     <div class="card-header">Jumlah Aspirasi</div>
+                        <div class="card-body">
+                        <img src="img-bg/icon_1.png" class="opacity-50" style="max-width: 50px" alt="">
+                        <p class="card-text mt-2 fw-bold">1000</p>
+                     </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+                  <div class="card text-dark bg-white mx-3 mb-3" style="max-width: 18rem;">
+                     <div class="card-header">Aspirasi Diproses</div>
+                        <div class="card-body">
+                        <img src="img-bg/icon_2.png" class="opacity-50" style="max-width: 50px" alt="">
+                        <p class="card-text mt-2 fw-bold">1000</p>
+                     </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+                  <div class="card text-dark bg-white mx-3" style="max-width: 18rem;">
+                     <div class="card-header">Aspirasi Selesai</div>
+                        <div class="card-body">
+                        <img src="img-bg/icon_3.png" class="opacity-50" style="max-width: 50px" alt="">
+                        <p class="card-text mt-2 fw-bold">1000</p>
+                     </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+                  <div class="card text-dark bg-white mx-3 mb-3" style="max-width: 18rem;">
+                     <div class="card-header">Jumlah View</div>
+                        <div class="card-body">
+                        <img src="img-bg/icon_4.png" class="opacity-50" style="max-width: 50px" alt="">
+                        <p class="card-text mt-2 fw-bold">1000</p>
+                     </div>
+                  </div>
+            </div>
+         </div>
+      </div>
+   </main>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,71 @@
+  <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fixed top navbar example · Bootstrap v5.1</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/navbar-fixed/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+      body {
+         min-height: 75rem;
+         padding-top: 4.5rem;
+      }
+
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (max-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+
+        .title{
+            font-size: 11px;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">ASM SUL-SEL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <hr class="mt-1 bg-light">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Cara-Pengajuan</a>
+        </li>
+        <hr class="mt-1 bg-light">
+        <li class="nav-item">
+          <a class="nav-link px-4 active btn btn-sm btn-success">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+  <div class="content">
+    @yield('content')
+  </div>
+ 
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>  
+  </body>
+</html>
