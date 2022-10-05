a0025c9b89a43bf54ac5e4d05ff2d123f40430dd
patrik pawa <patrikwilliam001@gmail.com>
2022-10-05 10:39:56 +0800

    add new file

--- resources/views/users/login/index.blade.php
+++ resources/views/users/login/index.blade.php
@@ -1,7 +1,33 @@
-@extends('users.layouts.main')
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login - Ruang aspirasi masyarakat</title>
 
-@section('content')
-   <main class="form-signin">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+    <main class="form-signin">
       <form action="/login" method="post">
          @csrf
          <img class="mb-3" src="img-bg/ss.png" alt="" width="72">
@@ -24,5 +50,8 @@
          </div>
          <p class="mt-5 mb-3 text-muted">&copy; KKL-UNDIPA 2022 - {{ date('Y') }}</p>
       </form>
-   </main>
-@endsection
+    </main> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+  </body>
+</html>
